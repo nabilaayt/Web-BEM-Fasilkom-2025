2ae4d51ba37c986dd410b5c0721f611fcc888375
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2025-09-21 23:10:51 +0700

    chore: init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-black justify-center items-center">Selamat Mengoding :D</h1>
+    </>
+  )
+}
+
+export default App;
