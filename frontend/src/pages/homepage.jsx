71f7aadc440515d7830546fedf8038c18900b500
mnabilrafasya <mnabilrafasya03@gmail.com>
2025-11-12 05:51:07 +0700

    filosofi

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 // import Navbar from "../../components/N_F/Navbar/Navbar";
 // import Footer from "../../components/N_F/Footer/Footer";
-import Hero from "../components/Home/Hero";
-import Who_are_we from "../components/Home/Who_are_we";
-import Inside_BEM_Apps from "../components/Home/Inside";
+import Hero from "../components/home/Hero";
+import Who_are_we from "../components/home/Who_are_we/Index.jsx";
+import Inside_BEM_Apps from "../components/home/Inside/index.jsx";
 
 const Homepage = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:p-0 p-0">
       <div className="w-full">
-        {/* <Navbar /> */}
         <Hero />
         <Who_are_we />
         <Inside_BEM_Apps />
-        {/* <Footer /> */}
       </div>
     </main>
   );
